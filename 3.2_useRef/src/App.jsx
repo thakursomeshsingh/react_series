91975a4acb68eeda257c4fa0c190a60ad5ea4271
thakursomeshsingh <someshkumarsingh1999@gmail.com>
2024-07-11 14:15:01 +0530

    nothing

--- 3.2_useRef/src/App.jsx
+++ 3.2_useRef/src/App.jsx
@@ -4,10 +4,20 @@ import './App.css'
 function App() {
   const inputRef = useRef(null);
 
+  // example 2
+  const handleButtonClick = () => {
+    const inputValue = inputRef.current.value;
+    alert(`Input value: ${inputValue}`);
+  }
+
   return (
    <>
    <div className="App">
     <h1>useRef in react</h1>
+
+    {/* example 1 */}
+    <input ref={inputRef} type="text" />
+    <button onClick={handleButtonClick}>Get input</button>
     </div></>
   )
 }
